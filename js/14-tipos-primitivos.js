cfbf08196ba1f5f8044a922f6e967ec652bf9236
IOswaldoZarzaMorales <oswaldomorales26@hotmail.com>
2023-12-08 14:55:22 -0600

    first commit

---
+++ js/14-tipos-primitivos.js
@@ -0,0 +1,35 @@
+// Tipos primitivos
+
+//Number
+4
+
+//string
+"Hola mundo";
+'Hola mundo';
+
+
+//booleanos 
+true;
+false;
+
+// nulo y undefined
+
+null;
+undefined;
+
+//null, undefined, false , 0 = todas son falsy es la unica similitud
+if (null) {
+    console.log("cumple")
+} else {
+    console.log("no cumple")
+}
+
+//scope es el alcance de nuestras variables
+
+let nombre = 'Daniel';
+let invitadxsExtras =2;
+let esMayorEdad = true;
+let edad= 24;
+let invitadxExpecial = null;
+let horaDeSalida = undefined;
+
